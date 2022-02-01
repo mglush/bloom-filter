824f7cde47e2e3939999c28051f4941a765b1286
Michael Glushchenko <glushchenko@cs.ucsb.edu>
2022-01-31 17:04:32 -0800

    combined testHashTable1.cpp and testHashTable2.cpp into a single test file by the name of testHashTable.cpp

--- testHashTable.cpp
+++ testHashTable.cpp
@@ -1,27 +1,58 @@
-// testHashTable2.cpp
+// testHashTable.cpp
 // this test file tests the following HashTable functions:
+// strToInt()
+// hash()
 // insert()
 // remove()
 // find()
 
 #include <cassert>
 #include <string>
-#include <sstream>
+#include <iostream>
 
 #include "testHashTable.h"
 #include "testFramework.h"
 #include "HashTable.h"
 
 int main() {
-    std::cout << "Running testHashTable2 file!" << std::endl << std::endl;
+    std::cout << "Running testHashTable file!" << std::endl << std::endl;
     runAllTests();
-    std::cout << "Finished running testHashTable2 file!" << std::endl << std::endl;
+    std::cout << "Finished running testHashTable file!" << std::endl << std::endl;
 }
 
 void runAllTests() {
+    test_strToInt();
+    test_hash();
+    //---------------
     test_insert();
     test_remove();
     test_find();
+}
+
+void test_strToInt() {
+    START_TEST("test_strToInt");
+
+    HashTable myTable(13);
+    assertEquals(0, myTable.strToInt(""), "testing empty string");
+    assertEquals(3135, myTable.strToInt("ab"), "testing ab");
+    assertEquals(3105, myTable.strToInt("ba"), "testing ba");
+    assertEquals(3016800, myTable.strToInt("aabb"), "testing aabb");
+    assertEquals(2986080, myTable.strToInt("bbaa"), "testing bbaa");
+
+    END_TEST("test_strToInt");
+}
+
+void test_hash() {
+    START_TEST("test_hash");
+
+    HashTable myTable(13);
+    assertEquals(0 % 13, myTable.hash(""), "hashing an empty string");
+    assertEquals(3135 % 13, myTable.hash("ab"), "hashing 'ab'");
+    assertEquals(3105 % 13, myTable.hash("ba"), "hashing 'ba'");
+    assertEquals(3016800 % 13, myTable.hash("aabb"), "hashing 'aabb'");
+    assertEquals(2986080 % 13, myTable.hash("bbaa"), "hashing 'bbaa'");
+
+    END_TEST("test_hash");
 }
 
 void test_insert() {
